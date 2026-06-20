55e1d849fafe38b761381199e56a4703f40fc8c0
Sumeth Vindunu <164194470+SumethVindunu@users.noreply.github.com>
2026-06-20 20:37:50 +0530

    implement Compare Designs

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -38,10 +38,10 @@ const Navbar = () => {
                     </div>
 
                     <ul className="links">
-                        <a href="#">Product</a>
-                        <a href="#">Pricing</a>
-                        <a href="#">Community</a>
-                        <a href="#">Enterprise</a>
+                        {/*<a href="#">Product</a>*/}
+                        {/*<a href="#">Pricing</a>*/}
+                        {/*<a href="#">Community</a>*/}
+                        {/*<a href="#">Enterprise</a>*/}
                     </ul>
                 </div>
 
